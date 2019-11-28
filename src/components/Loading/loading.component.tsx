1b90baa0856f9d7b3f3377902cefd72a83b79732
maneptha <maneptha@gmail.com>
2019-11-28 11:31:08 +0100

    Loading component takes react loadable props.
    To be used as suspense in react loadable.

--- src/components/Loading/loading.component.tsx
+++ src/components/Loading/loading.component.tsx
@@ -1,21 +1,32 @@
 import React, { PropsWithChildren } from "react";
 import "./loading.styles.scss";
 import { addClassNames } from "../components";
+import { LoadingComponentProps } from "react-loadable";
 
-export const Loading = ({
+export function LoadableLoading(props: LoadingComponentProps) {
+  return <Loading loadableProps={props} />;
+}
+
+export function Loading({
   className,
   children,
+  loadableProps,
   ...props
-}: PropsWithChildren<{ className?: string }>) => (
-  <div className="components-loading">
-    <div
-      className={addClassNames("components-loading__spinner", className)}
-      {...props}
-    >
-      <div className="double-bounce1" />
-      <div className="double-bounce2" />
-    </div>
+}: PropsWithChildren<{
+  className?: string;
+  loadableProps?: LoadingComponentProps;
+}>) {
+  return (
+    <div className="components-loading">
+      <div
+        className={addClassNames("components-loading__spinner", className)}
+        {...props}
+      >
+        <div className="double-bounce1" />
+        <div className="double-bounce2" />
+      </div>
 
-    {children}
-  </div>
-);
+      {children}
+    </div>
+  );
+}
